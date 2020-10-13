3c41d17b547f4ee4b26219f24948467742a98a84
Katie Vogel <kvog983@gmail.com>
2020-10-12 22:03:44 -0400

    added explanatory comments

--- exp-app.js
+++ exp-app.js
@@ -1,6 +1,7 @@
-
+//array of doctor who themed search buttons to appear on the page
 var arrButtons = ["Ninth Doctor", "Tenth Doctor", "Eleventh Doctor", "Twelth Doctor", "Thirteenth Doctor", "Dalek", "Cyberman", "Adipose", "Missy", "Amy Pond", "Rory Williams", "Clara"];
 
+//the function that creates a new button with attributes and click behavior. the click behavior triggers a GET request to the Giphy API to search the keyword. specific characteristics are retrieved that allow the gifs to paused and started again
 function makeOneButton(label) {
     var newButton = document.createElement('button');
     $(newButton).attr("class", "gif-button btn btn-primary");
@@ -43,9 +44,9 @@ function makeOneButton(label) {
                 }
             })
     });
-    document.querySelector('div').append(newButton);
+    document.querySelector('.new-buttons').append(newButton);
 }
-
+//function thats uses the makeOneButton function to make the default buttons from the initial array. 
 function makeButtons() {
     for (var i = 0; i < arrButtons.length; i++) {
         makeOneButton(arrButtons[i]);
@@ -53,10 +54,11 @@ function makeButtons() {
 }
 makeButtons();
 
+//clears search box after searching the gif keyword
 function clearInput() {
     document.querySelector("form").reset();
 }
-
+//another GET request on the gif search button to search the API for whatever keyword was entered. pulls back the same characteristics that the other buttons and then creates the new button
 $("#gif-me").on("click", function () {
     var gifSearch = $("input.form-control").val();
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=0flPku5i7SaRbjTl02ZnhKrnYHH6Z4uk&q=Doctor%20Who" + gifSearch + "&limit=10&offset=0&rating=G&lang=en";
